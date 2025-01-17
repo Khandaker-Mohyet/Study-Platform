de47cc74a77a3868d1333a62651e4a22d80c87dc
Khandaker-Mohyet <mohyet008@gmail.com>
2025-01-18 01:37:57 +0600

    make admin

--- src/Pages/Dashboard/Admin/Users/Users.jsx
+++ src/Pages/Dashboard/Admin/Users/Users.jsx
@@ -14,6 +14,23 @@ const Users = () => {
     }
   })
 
+  const handleMakeAdmin = user => {
+    axiosSecure.patch(`/users/admin/${user._id}`)
+      .then(res => {
+        console.log(res.data)
+        if (res.data.modifiedCount > 0) {
+          refetch();
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: `${user.name} is an Admin Now!`,
+            showConfirmButton: false,
+            timer: 1500
+          });
+        }
+      })
+  }
+
 
   const handleDeleteUser = user => {
     Swal.fire({
@@ -69,7 +86,7 @@ const Users = () => {
                 <td>{user.email}</td>
                 <td>
                   {user.role === 'admin' ? 'Admin' : <button
-                    // onClick={() => handleMakeAdmin(user)}
+                    onClick={() => handleMakeAdmin(user)}
                     className="btn btn-lg bg-orange-500">
                     <FaUsers className="text-white 
                                         text-2xl"></FaUsers>
