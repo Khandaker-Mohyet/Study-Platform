a9ac7dc11c87b2a5eef396a6f141cdd2e63a5fb8
Khandaker Mohyet <mohyet0007@gmail.com>
2025-01-24 12:13:30 +0600

    update card

--- src/Components/Google.jsx
+++ src/Components/Google.jsx
@@ -14,7 +14,7 @@ const Google = () => {
         console.log(result.user);
         const userInfo = {
           email: result.user?.email,
-          name: result.user?.displayName,
+          displayName: result.user?.displayName,
           photoURL: result.user?.photoURL,
           role: 'Student'
         }
